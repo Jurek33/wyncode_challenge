e1fa957da4bb7ecd43897b60252f1a7ebc0f708e
Yury Hrytsuk <jerzy.gricuk@yurys-air.attlocal.net>
2020-05-21 13:32:26 -0400

    Updates

---
+++ precache-manifest.1afa18f033b0ddfed2ad779c9c19dd56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc23f7278a51c23a5120902f818d4ab",
+    "url": "/wyncode-challenge/index.html"
+  },
+  {
+    "revision": "4922fee851ab34a63513",
+    "url": "/wyncode-challenge/static/css/main.a6d7d0c4.chunk.css"
+  },
+  {
+    "revision": "7060fffa5c5ffea81b4f",
+    "url": "/wyncode-challenge/static/js/2.0e31e9d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wyncode-challenge/static/js/2.0e31e9d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4922fee851ab34a63513",
+    "url": "/wyncode-challenge/static/js/main.4e2d2057.chunk.js"
+  },
+  {
+    "revision": "93de9079595225eefedb",
+    "url": "/wyncode-challenge/static/js/runtime-main.286541e8.js"
+  }
+]);
